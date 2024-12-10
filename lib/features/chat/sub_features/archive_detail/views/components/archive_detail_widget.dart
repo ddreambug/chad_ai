eef8407b9eca1854093f57466f69aaa2ea529aeb
Dest <86515936+ddreambug@users.noreply.github.com>
2024-12-10 17:48:11 +0700

    add title description to notify user about archived chat and all chat

--- lib/features/chat/sub_features/archive_detail/views/components/archive_detail_widget.dart
+++ lib/features/chat/sub_features/archive_detail/views/components/archive_detail_widget.dart
@@ -1,7 +1,10 @@
+import 'package:chad_ai/configs/themes/main_color.dart';
 import 'package:chad_ai/features/chat/sub_features/archive_detail/controllers/archive_detail_controller.dart';
+import 'package:chad_ai/shared/styles/custom_text_style.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:chad_ai/features/chat/sub_features/chat_detail/views/components/chat_detail_item.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ArchiveDetailWidget extends StatelessWidget {
   const ArchiveDetailWidget({super.key});
@@ -16,6 +19,22 @@ class ArchiveDetailWidget extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          Center(
+            child: Container(
+              margin: EdgeInsets.only(top: 5.w, bottom: 10.w),
+              width: 350.w,
+              decoration: BoxDecoration(color: Colors.white),
+              child: Text(
+                'Currently you can only see your chat history, stay tunes for further updates',
+                textAlign: TextAlign.center,
+                style: CustomTextStyle.w400.copyWith(
+                  color: MainColor.textGrey,
+                  fontSize: 12,
+                ),
+              ),
+            ),
+          ),
+
           // ListView
           Expanded(
             child: Obx(
--- lib/features/chat/sub_features/chat_detail/views/components/chat_detail_widget.dart
+++ lib/features/chat/sub_features/chat_detail/views/components/chat_detail_widget.dart
@@ -1,11 +1,14 @@
+import 'package:chad_ai/configs/themes/main_color.dart';
 import 'package:chad_ai/features/chat/sub_features/chat_detail/controllers/chat_detail_controller.dart';
 import 'package:chad_ai/features/chat/sub_features/chat_detail/views/components/chat_input.dart';
+import 'package:chad_ai/shared/styles/custom_text_style.dart';
 import 'package:chad_ai/utils/enums/enum.dart';
 import 'package:chad_ai/utils/services/utility_service.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:chad_ai/features/chat/sub_features/chat_detail/views/components/chat_detail_item.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ChatDetailWidget extends StatelessWidget {
   const ChatDetailWidget({super.key});
@@ -20,6 +23,32 @@ class ChatDetailWidget extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          // Infomessage
+          Obx(
+            () {
+              var chatLength = controller.chat.value!.history.length;
+
+              if (chatLength == 0) {
+                return Center(
+                  child: Container(
+                    margin: EdgeInsets.only(top: 20.w),
+                    width: 350.w,
+                    child: Text(
+                      'Un-Archived chat is temporary that could lead to lost of history when changing devices. \n\nConsider Archiving your chat if you plan to save it on our Database',
+                      textAlign: TextAlign.center,
+                      style: CustomTextStyle.w400.copyWith(
+                        color: MainColor.textGrey,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
+                );
+              } else {
+                return SizedBox.shrink();
+              }
+            },
+          ),
+
           // ListView
           Expanded(
             child: Obx(
